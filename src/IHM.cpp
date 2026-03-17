6bf4d73c11fa99110cacc48c547ffb36782c91a2
PSYAMEN <nicolas.2.brebisson@gmail.com>
2026-03-17 11:16:53 +0100

    ihm fix

--- src/IHM.cpp
+++ src/IHM.cpp
@@ -1,13 +1,13 @@
 #include "IHM.h"
 
-IHM::Controls getInputs(){
+Controls IHM::getInputs(){
     Controls c;
     c.up=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up);
     c.down=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down);
     c.right=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right);
     c.left=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left);
-    c.pause=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space);
-    c.select=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter);
+    c.pause=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape);
+    c.select=sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space);
     return c;
 }
 
