452771d08cc385f71af8aeadffdfda1a14e34087
WhyEleven <charlie.merchadou@etu.univ-lyon1.fr>
2026-03-17 12:11:38 +0100

    enemy

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,1 +1,19 @@
 #include "Enemy.h"
+
+Enemy::Enemy(bool a){
+    hp=1;
+    attack=0;
+    speed=0.05;
+    position.x=rand()%800;
+    position.y=rand()%800;
+    isAlive=a;
+}
+
+Enemy::~Enemy(){}
+
+void Enemy::move(const Position& player){
+    Position dir;//direction
+    dir.posX = player.posX - position.posX; dir.posY = player.posY - position.posY; 
+    float dist = std::sqrt(dir.posX * dir.posY + dir.posY * dir.posY); //norme du vecteur 
+    if (dist > 0) position += dir / dist * speed; //normalisation puis * vitesse
+}
--- src/Enemy.h
+++ src/Enemy.h
@@ -1,14 +1,30 @@
 #ifndef _ENEMY_H
 #define _ENEMY_H
 
+//nos include
+#include "Bullet.h"
 
 
 class Enemy{
 private:
-
+    unsigned int sprite;
+    unsigned int height,width;
+    unsigned int hp;
+    unsigned int attack;
+    unsigned int experience;
+    unsigned int speed;
 public:
-
+    Position position;
+    bool isAlive;
+    Enemy* next;
+    Enemy(bool a=false);
+    Enemy(int health=1,int damage=0,bool isAlive=false);
+    ~Enemy();
+    void shoot(Bullet &bullets);
+    bool takeDamageBullet(Bullet &bullets);
+    void move(const Position &player);
 };
+
 
 
 
