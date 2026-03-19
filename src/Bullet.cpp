3e120f4220753ba023acf938c4e0406d34635317
WhyEleven <charlie.merchadou@etu.univ-lyon1.fr>
2026-03-19 16:16:36 +0100

    fix operator const

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -5,28 +5,28 @@ float  Position::length(){
     return sqrt(posX*posX+posY*posY);
 }
 
-Position Position::operator +(Position p2){
+Position Position::operator +(const Position & p2) const {
     Position p;
     p.posX=posX+p2.posX;
     p.posY=posY+p2.posY;
     return p;
 }
 
-Position Position::operator -(Position p2){
+Position Position::operator -(const Position & p2) const {
     Position p;
     p.posX=posX-p2.posX;
     p.posY=posY-p2.posY;
     return p;
 }
 
-Position Position::operator *(float q){
+Position Position::operator *(const float q) const {
     Position p;
     p.posX=posX*q;
     p.posY=posY*q;
     return p;
 }
 
-Position Position::operator /(float q){
+Position Position::operator /(const float q) const {
     if(q!=0){
         Position p;
         p.posX=posX/q;
