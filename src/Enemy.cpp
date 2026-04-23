f98ecbac7b75cfa32e0b557b3a9ec3f3a2309158
WhyEleven <charlie.merchadou@etu.univ-lyon1.fr>
2026-04-23 15:35:59 +0200

    optimisation 3 fonctions move de enemy

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -28,8 +28,20 @@ const Stats& Enemy::getStats()const{return stats;}
 void Enemy::move(Position& player){
     bool shooter= (type=="Archer")||(type=="Spearman");//...
     bool deserter= (type=="Rabbit");//...
-    if(position.posX<50){position.posX=50.f;}else if(position.posX>750){position.posX=750.f;} //prévient la sortie de case
-    if(position.posY<50){position.posY=50.f;}else if(position.posY>750){position.posY=750.f;} 
+    if(int(position.posX)<50){ //prévient la sortie de case
+        position.posX=50.f;
+        return;
+    }else if(int(position.posX)>750){
+        position.posX=750.f;
+        return;
+    }
+    if(int(position.posY)<50){
+        position.posY=50.f;
+        return;
+    }else if(int(position.posY)>750){
+        position.posY=750.f;
+        return;
+    } 
     if(shooter){moveShooter(player);return;}
     if(deserter){moveDeserter(player);return;}
     moveAgro(player);
@@ -44,18 +56,14 @@ void Enemy::moveAgro(Position &player){ // fonce sur le joueur
 void Enemy::moveShooter(Position &player){ // move de manière circulaire et reste à distance du joueur
     float maxDist=200; // taille de l'écart entre joueur et l'ennemy
     Position dir = player - position; //direction vers le joueur
-    //bool q =(reinterpret_cast<std::uintptr_t>(this) >> 4) & 1;//vaut 1 ou 0, pour le sens de l'ennemy, constant et identifié par son adresse
-    float x=-1*(rotationSide*2-1)*dir.posY; // on ajoute +ou- 90 deg au vecteur direction
-    dir.posY=(rotationSide*2-1)*dir.posX;
-    dir.posX=x;
-    //remarque: on ne change pas la norme du vecteur donc on peut encore savoir si on est trop proche 
-    if(dir.length()<=maxDist){//on ajoute un angle pi/4 pour s'éloigner
-        x=(dir.posX-(rotationSide*2-1)*dir.posY)*0.70710678118;
-        dir.posY=(dir.posX+(rotationSide*2-1)*dir.posY)*0.70710678118;
-        dir.posX=x;
-    }
     float dist = dir.length(); //norme du vecteur 
-    if (dist > 0) position =position+ (dir / dist) * stats.playerSpeed; //normalisation puis * vitesse
+    if (dist<=1e-1) return;
+    // on ajoute +ou- pi/2 rad au vecteur direction, ou 3pi/4 si trop proche 
+    if(dist<=maxDist){
+        dir.rotate(-0.70710678118,(rotationSide*2-1)*0.70710678118); // cos(3pi/4)=-sin(3pi/4)=-0.70710678118  
+    }else{dir.rotate(0,(rotationSide*2-1));}
+  
+    position = position + (dir / dist) * stats.playerSpeed; //normalisation puis * vitesse
 }
 
 void Enemy::moveDeserter(Position &player){//move qui fuit le joueur et ne se coinse pas 
@@ -67,11 +75,10 @@ void Enemy::moveDeserter(Position &player){//move qui fuit le joueur et ne se co
     dir = dir / dist; //normalisation
 
     const int n = 24;
-    float bestScore = -1e9; // score du meilleur rayon
+    float bestScore = -1e9; // score du meilleur rayon initialisé très bas
     Position bestDir = -dir; // fallback = fuite directe
-
-    for(int i = 0; i < n; i++){
-        float angle = i * (2*M_PI/n); // 330° divisées en n rayons
+    for(int i = 1; i < n; i++){
+        float angle = i * (2*M_PI/n); // 360° divisées en (n-1) rayons
         
         float c = cos(angle);
         float s = sin(angle);
@@ -96,7 +103,7 @@ void Enemy::moveDeserter(Position &player){//move qui fuit le joueur et ne se co
 }
 
 
- void Enemy::shoot(Bullet &bullets, unsigned int nbP, Player* tabP)const{//à update dans player.cpp!!!!!
+ void Enemy::shoot(Bullet &bullets, unsigned int nbP, Player* tabP)const{
     //if(nbP==0){bullets.damage=0;return;}
     Position dist=tabP[0].getPosition()-position;
     float distMin=800;
@@ -110,6 +117,7 @@ void Enemy::moveDeserter(Position &player){//move qui fuit le joueur et ne se co
             canshoot=true;
         }
     }
+    if(distMin>=400) return; // il faut étre proche du joueur pour lui tirer dessus
     if(canshoot){
         Position speed=(tabP[idMin].getPosition()-position)/(tabP[idMin].getPosition()-position).length();
         bullets.pos=position;
